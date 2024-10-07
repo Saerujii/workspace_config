4a946fa4d07d450ddc04646399b487770950cc39
Saerujii <sergeysmik.yt@gmail.com>
2024-10-07 13:19:09 +0200

    Changes in maventest.sh

--- bashscripts/maventest.sh
+++ bashscripts/maventest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <TestClassName>"
+  echo "Usage: maventest <TestClassName>"
   exit 1
 fi
 
 # Run the Java command with the provided argument
+# mvn package | rg "time" # Add argument for optional packaging
+echo
 echo "-------------------------------"
 java -cp target/test-classes:/home/saeru/Desktop/DAW2/Java/php-to-java/target/classes com.phpToJava."$1"
 echo "-------------------------------"
+echo
