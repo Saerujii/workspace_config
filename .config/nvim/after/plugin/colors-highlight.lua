9770f07e58fbc68ab96057c6d131c06e39ce2bb0
Saerujii <sergeysmik.yt@gmail.com>
2024-10-23 00:06:06 +0200

    Fixed weird css C-f format issue

--- .config/nvim/after/plugin/colors-highlight.lua
+++ .config/nvim/after/plugin/colors-highlight.lua
@@ -3,7 +3,11 @@ vim.opt.termguicolors = true
 -- require('nvim-highlight-colors').setup({})
 
 local ccc = require("ccc")
-ccc.setup {}
+ccc.setup {
+    highlighter = {
+        lsp = false;
+    }
+}
 
 vim.api.nvim_create_autocmd("BufEnter", {
     pattern = "*.css",
