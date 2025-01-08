b2eb5a64859c9f20c7d2bdc5272f50d247651611
Saerujii <sergeysmik.yt@gmail.com>
2025-01-08 13:37:13 +0100

    maventest.sh refactored, now it also gets dependencies from maven, so that it can run them in tests

--- bashscripts/maventest.sh
+++ bashscripts/maventest.sh
@@ -9,6 +9,6 @@ fi
 # mvn package | rg "time" # Add argument for optional packaging
 echo
 echo "-------------------------------"
-java -cp target/test-classes:/home/saeru/Desktop/DAW2/Java/php-to-java/target/classes com.phpToJava."$1"
+java -cp "$(mvn dependency:build-classpath -q -DincludeScope=runtime -Dmdep.outputFile=/dev/stdout)":target/test-classes:/home/saeru/Desktop/DAW2/Java/php-to-java/target/classes com.phpToJava."$1"
 echo "-------------------------------"
 echo
