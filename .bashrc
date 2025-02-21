14e5b7b855deb4d018b78df17ede25977687bfca
Saerujii <sergeysmik.yt@gmail.com>
2025-02-21 12:01:41 +0100

    KUBE_EDITOR variable, maven build script changed

--- .bashrc
+++ .bashrc
@@ -29,3 +29,5 @@ export PATH="$PATH:/home/saeru/.local/share/gem/ruby/3.3.0/bin"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export KUBE_EDITOR=nvim
--- bashscripts/mavenbuild.sh
+++ bashscripts/mavenbuild.sh
@@ -6,6 +6,12 @@ mvn package
 echo "-------------------------------"
 echo
 
+if [ "$1" == "deploy" ]; then
+    mv target/*1.0-SNAPSHOT.war target/"$(basename "$PWD")".war
+    sudo mv target/"$(basename "$PWD")".war /usr/share/tomcat10/webapps/
+    sudo systemctl restart tomcat10
+fi
+
 if [ "$1" == "run" ]; then
     source "$HOME/bashscripts/mavenrun.sh"
 fi
